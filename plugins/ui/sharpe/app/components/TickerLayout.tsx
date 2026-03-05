2209dc3bb9f1c71e0af53cdc3f6a512371d91640
Balint Csikos <balint.csikos.priv@gmail.com>
2026-03-05 14:51:15 +0100

    Adding initial page layout and component library (sharpe ui only).

---
+++ plugins/ui/sharpe/app/components/TickerLayout.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import TickerList from './TickerList'
+import TickerDetails from './TickerDetails'
+
+export default function TickerLayout() {
+  return (
+    <div className="flex min-h-screen flex-col gap-6 bg-background p-4 sm:p-6 lg:flex-row lg:gap-8">
+      {/* Left/Top Column - TickerList */}
+      <div className="w-full lg:w-1/2">
+        <TickerList />
+      </div>
+
+      {/* Right/Bottom Column - TickerDetails */}
+      <div className="w-full lg:w-1/2">
+        <TickerDetails />
+      </div>
+    </div>
+  )
+}
